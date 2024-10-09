ce577ed96e3ac3d1019e9c130af5a44018870078
Heewon Yu <gmldnjs0317@gmail.com>
2024-10-09 19:14:55 +0900

    cpp01_practice(cout)

---
+++ daily_practice/cpp01_practice(cout)/cout_example_1.cpp
@@ -0,0 +1,13 @@
+// 1.cout_example_1
+// answer:My name is À¯Èñ¿ø.
+//        My student ID is 231583.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "My name is À¯Èñ¿ø." << endl;
+  cout << "My student ID is 231583." << endl;
+  return 0;
+}
---
+++ daily_practice/cpp01_practice(cout)/cout_example_2.cpp
@@ -0,0 +1,12 @@
+// 2.cout_example_2
+// answer:My name is À¯Èñ¿ø.
+//        My student ID is 231583.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "My name is À¯Èñ¿ø.\nMy student ID is 231583.";
+  return 0;
+}
