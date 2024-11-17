a669741cb9b0b604e02473e83203d0a3e9d604b0
Heewon Yu <gmldnjs0317@gmail.com>
2024-11-17 19:12:28 +0900

    template으로 구현

--- daily_practice/cpp14_practice_class/Calculator_with_a_class.cpp
+++ daily_practice/cpp14_practice_class/Calculator_with_a_class.cpp
@@ -3,23 +3,29 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
 class my_calc {
  private:
-  int num1, num2;
+  T num1, num2;
 
  public:
-  my_calc(int n1, int n2) : num1(n1), num2(n2) {}
+  my_calc(T num1, T num2) : num1(num1), num2(num2)
+  {}
 
-  int add() { return num1 + num2; }
-  int sub() { return num1 - num2; }
-  int mul() { return num1 * num2; }
-  int div() {
-    if (num2 != 0) return num1 / num2;
+  T add() { return num1 + num2; }
+  T sub() { return num1 - num2; }
+  T mul() { return num1 * num2; }
+  T div() {
+    if (num2 == 0) {
+      cout << "Cannot divide by ";
+      return num2;
+    }
+    return num1 / num2;
   }
 };
 
 int main() {
-  my_calc c(10, 0);
+  my_calc<int> c(10, 20);
 
   cout << c.add() << endl;
   cout << c.sub() << endl;
